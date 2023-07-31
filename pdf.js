43f1dd9dc3138e49d7b7b07a60296280aa161060
akshukatkar <akshukatkar@gmail.com>
2023-08-01 03:59:03 +0530

    Create pdf.js

---
+++ pdf.js
@@ -0,0 +1,16 @@
+
+{
+  "config": {
+      "modulename" : "PDF Viewer",
+      "version" : "1.0",
+      "head": "Bribery warning I",
+      "source": "Supplied",
+      "file": "news://www.smh.com.au/interactive/modules/pdfviewer/pdfs/P2/Paladin1.pdf",
+      "defaultpage": 1,
+      "mobileimage": "https://www.smh.com.au/interactive/modules/pdfviewer/json/p2/Paladin1_mobile.jpg",
+      "desktopimage": "https://www.smh.com.au/interactive/modules/pdfviewer/json/p2/Paladin1_desktop.jpg",
+      "shape": "portrait",
+      "download": "",
+      "print": ""
+  }
+}
