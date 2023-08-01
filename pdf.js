f0323e47350ba63a22321347f1ab09ca44bde315
akshukatkar <akshukatkar@gmail.com>
2023-08-01 21:00:29 +0530

    Update pdf.js

--- pdf.js
+++ pdf.js
@@ -3,14 +3,14 @@
   "config": {
       "modulename" : "PDF Viewer",
       "version" : "1.0",
-      "head": "Bribery warning I",
-      "source": "Supplied",
+      "head": "Bribery<akshay>katkar warning I",
+      "source": "Suppl<akshay>katkaried",
       "file": "news://www.smh.com.au/interactive/modules/pdfviewer/pdfs/P2/Paladin1.pdf",
       "defaultpage": 1,
       "mobileimage": "https://www.smh.com.au/interactive/modules/pdfviewer/json/p2/Paladin1_mobile.jpg",
       "desktopimage": "https://www.smh.com.au/interactive/modules/pdfviewer/json/p2/Paladin1_desktop.jpg",
       "shape": "portrait",
-      "download": "",
-      "print": ""
+      "download": "<akshay>katkar",
+      "print": "<akshay>katkar"
   }
 }
