777412d959bc7109920678af0fd8241f5892fdf3
akshukatkar <akshukatkar@gmail.com>
2025-07-01 09:58:48 +0530

    Create momom.js

---
+++ momom.js
@@ -0,0 +1,18 @@
+{
+  "InputImages": {
+    "BigSmileImageUrl": "https://example.com/images/big_smile_input.jpg",
+    "SmallSmileImageUrl": "https://example.com/images/small_smile_input.jpg",
+    "NeutralFaceImageUrl": "https://example.com/images/neutral_face_input.jpg"
+  },
+  "ResultImages": {
+    "FaceSmilingAfterTreatment": "https://example.com/results/smiling_after_treatment.jpg",
+    "FaceSmilingBeforeTreatment": "https://example.com/results/smiling_before_treatment.jpg",
+    "FaceNeutralAfterTreatment": "https://example.com/results/neutral_after_treatment.jpg",
+    "FaceNeutralBeforeTreatment": "https://example.com/results/neutral_before_treatment.jpg"
+  },
+  "SimulationDetails": {
+    "PatientId": "P12345",
+    "SimulationDate": "2025-07-01",
+    "TreatmentPlan": "Orthodontic correction and veneers"
+  }
+}
